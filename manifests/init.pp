2f30c106c27cccd89a6d3c092afc1d84512675e3
Jan Kaufman <jan.kaufman@virtualmaster.com>
2012-12-01 20:39:56 +0100

    Ubuntu precise with PPA

--- manifests/init.pp
+++ manifests/init.pp
@@ -32,11 +32,11 @@ class nodejs(
       include 'apt'
 
       # Only use PPA when necessary.
-      if $::lsbdistcodename != 'Precise'{
-        apt::ppa { 'ppa:chris-lea/node.js':
-          before => Anchor['nodejs::repo'],
-        }
+      # if $::lsbdistcodename != 'Precise'{
+      apt::ppa { 'ppa:chris-lea/node.js':
+         before => Anchor['nodejs::repo'],
       }
+      # if }
     }
 
     'Fedora', 'RedHat', 'CentOS', 'Amazon': {
